26d81e6854e176d01c9178f0a45fa35794b1efe1
nad220209-creator <nad220209@gmail.com>
2026-09-22 18:03:51 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Seller Information
+SELLER_NAME = "Muhammad Naveed Arshad"
+WHATSAPP_NUMBER = "03374633605"
+WHATSAPP_LINK = "https://wa.me/923374633605"
+MAIN_DRIVE_FOLDER_ID = "1NPYh-JHxjxF_kyu1ibkTO-AWhRCIJVmP"
+
+# Hardcoded OAuth & API Configuration
+CLIENT_ID = "295426809796-g7ij8hpd6c1bne47eitj0ilhbjqtfa5m.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
+REFRESH_TOKEN = "1//04-lxAxN2RYljCgYIARAAGAQSNwF-L9Ir_SwrAvwFZoNn-FumsqBkX5JfeWoMXVbUlS6g0-JvybhUcfclRSpQp3v-HYIgFxb4fq8"
+TOKEN_URL = "https://oauth2.googleapis.com/token"
